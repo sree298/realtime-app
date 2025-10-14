61c7fd1b532e4f9c1a1f1090fc1371b7ac51e7b2
Srinivasa Rao <pasumarthisrinu123@gmail.com>
2025-10-14 05:01:51 +0000

    updated

--- src/main/java/com/example/realtimeapp/RealtimeAppApplication.java
+++ src/main/java/com/example/realtimeapp/RealtimeAppApplication.java
@@ -2,12 +2,18 @@ package com.example.realtimeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RealtimeAppApplication {
+public class RealtimeAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RealtimeAppApplication.class, args);
     }
-}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RealtimeAppApplication.class);
+    }
+}
